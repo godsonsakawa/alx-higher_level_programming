32652c33c9954fd8985127f195853ab01efc00c7
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-12 15:54:29 +0300

    Dynamic Scripts

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
         There is no return value
     """
     import sys
-    no_of_arguments = len(sys.argv) - 1
-    if (no_of_arguments == 0):
+    no_of_args = len(sys.argv) - 1
+    if (no_of_args == 0):
         print("0 arguments.")
-    elif (no_of_arguments == 1):
+    elif (no_of_args == 1):
         print("1 argument:")
     else:
-        print("{:d} arguments:".format(no_of_arguments))
-    for argNo in range(no_of_arguments):
+        print("{:d} arguments:".format(no_of_args))
+    for argNo in range(no_of_args):
         print("{}: {}".format(argNo + 1, sys.argv[argNo + 1]))
