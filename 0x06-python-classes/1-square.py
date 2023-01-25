7f2b49820dc880abc672580cb841f05f0b2a41b8
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-25 20:24:53 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Define a class square
+"""
+
+class Square:
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new square
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
