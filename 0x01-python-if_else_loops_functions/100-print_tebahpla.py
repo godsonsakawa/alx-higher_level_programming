d3f3c126d2d785ecb56d30b15a38dadec5d17e7a
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-29 04:02:02 +0300

    Alphabet in reverse order, alternating upper and lowercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 """
 print the alphabet in reverse order alternating uppercase and lowercase.
 """
-i = 0;
-for c in range(ord('z'), ord('a') -1, -1):
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
