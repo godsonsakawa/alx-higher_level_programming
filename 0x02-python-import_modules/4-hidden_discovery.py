4d51eb0de9b1a33d3e618e9643c43a9e029df84c
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-31 10:11:42 +0300

    Program that prints names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     """prints all the names defined by the compiled module `hidden_4.pyc`"""
     import hidden_4
+    
     for names in dir(hidden_4):
         if (names[:2] != "__"):
             print(names)
