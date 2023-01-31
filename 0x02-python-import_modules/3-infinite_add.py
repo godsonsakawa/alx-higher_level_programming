1fa04e5f53b0684cca2fc5ca51fe2bfe9a4ce949
Godson Sakawa <godsonsakawa@gmail.com>
2023-01-31 09:47:29 +0300

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """prints the result of the addition of all arguments
-
-    Args:
-        system arguements
-
-    Returns:
-        There is no return value
+    """prints the result of the addition of all arguments.
     """
     import sys
+    
     sum_of_arguments = 0
     for argNo in range(len(sys.argv) - 1):
         sum_of_arguments += int(sys.argv[argNo + 1])
