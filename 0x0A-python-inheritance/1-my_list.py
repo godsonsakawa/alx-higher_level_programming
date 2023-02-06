377f219ec5a3242611fc66dc97aadc71ce4eb642
Godson Sakawa <godsonsakawa@gmail.com>
2023-02-06 19:18:15 +0300

    A class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines an inherited list class Mylist.
+"""
+
+
+class MyList(list):
+    """
+    Implements sorted printing for the built-in list class.
+    """
+
+    def print_sorted(self):
+        """
+        Prints a list in a sorted ascending order.
+        """
+        print(sorted(self))
