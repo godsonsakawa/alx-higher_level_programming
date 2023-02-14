17780c2887893d731251750e5639804c22911ffc
Godson Sakawa <godsonsakawa@gmail.com>
2023-02-14 14:48:42 +0300

    Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,13 @@ class Rectangle(Base):
     def area(self):
         """Returns the area of a rectangle."""
         return (self.__width * self.__height)
+
+    def display(self):
+        """Prints the Rectangle using the '#' character."""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        for h in range(self.height):
+            [print("#", end="") for w in range(self.width)]
+            print("")
