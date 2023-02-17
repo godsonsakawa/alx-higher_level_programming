f2aed29e3d23404626495fa05b8c512c63652d83
Godson Sakawa <godsonsakawa@gmail.com>
2023-02-18 02:54:23 +0300

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+import json
 
 
 class Base:
@@ -18,9 +19,23 @@ class Base:
 
         Args:
             id (int): The identity of the new Base.
+
+        The goal is to manage id attribute in all your future classes
+        and to avoid duplicating the same code(by extension, same bugs)
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of objects to a file.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
