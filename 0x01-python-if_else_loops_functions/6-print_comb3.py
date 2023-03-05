6efc0827e3b1eca4d831b4b952ca82cd3f2917cf
godson sakawa <godsonsakawa@gmail.com>
2023-03-05 05:23:58 +0300

    combination of digits - Task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(x + 1, 10):
-        if (x != 8 or y != 9):
-            print("{}{}, ".format(x, y), end="")
+
+"""Print all possible different combinations of two digits
+   `01` and `10` are considered same combo of the two digits `0` and `1`
+"""
+for digit1 in range(10):
+    for digit2 in range(digit1 + 1, 10):
+        if (digit1 != 8 or digit != 9):
+            print("{}{}, ".format(digit1, digit2), end="")
         else:
-            print("{}{}".format(x, y))
+            print("{}{}".format(digit1, digit2))
