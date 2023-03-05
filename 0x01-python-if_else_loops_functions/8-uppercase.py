f00052684bc4b3f8f48716a40fa1d32681274663
godson sakawa <godsonsakawa@gmail.com>
2023-03-05 06:19:39 +0300

    Convert string to uppercase - Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 def uppercase(str):
-    for i in range(len(str)):
-        uni_code = ord(str[i])
-        if (uni_code >= 97 and uni_code < 123):
+    """Print a string in uppercase."""
+    for letter in range(len(str)):
+        uni_code = ord(str[letter])
+        if (uni_code >= 97 and uni_code <= 122):
             uni_code = uni_code - 32
         print("{}".format(chr(uni_code)), end='')
-    print()
+    print("")
